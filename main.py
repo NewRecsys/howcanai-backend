0a4057b1c096f8f3ea817b86d79557eab3e2018c
Moomin Hyunseok Cho <moomin@constacts.com>
2023-07-10 15:28:23 +0900

    Fix: API

--- main.py
+++ main.py
@@ -12,21 +12,20 @@ class Completion(BaseModel):
 @app.post("/v1/chat/completions")
 def chat(completion: Completion):
     return {
-      "id": "cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7",
-      "object": "text_completion",
-      "created": 1589478378,
-      "model": "text-davinci-003",
-      "choices": [
-        {
-          "text": "이것은 howcan.ai의 답변입니다.",
-          "index": 0,
-          "logprobs": None,
-          "finish_reason": "length"
-        }
-      ],
+      "id": "chatcmpl-123",
+      "object": "chat.completion",
+      "created": 1677652288,
+      "choices": [{
+        "index": 0,
+        "message": {
+          "role": "assistant",
+          "content": "\n\nHello there, how may I assist you today?",
+        },
+        "finish_reason": "stop"
+      }],
       "usage": {
-        "prompt_tokens": 5,
-        "completion_tokens": 7,
-        "total_tokens": 12
+        "prompt_tokens": 9,
+        "completion_tokens": 12,
+        "total_tokens": 21
+      }
     }
-}
