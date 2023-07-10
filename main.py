318ee9311bd4a88d0db1d3ab595254457e25aede
Moomin Hyunseok Cho <moomin@constacts.com>
2023-07-10 16:00:17 +0900

    Feat: CORS

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://howcan.ai",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Completion(BaseModel):
     model: str
