a8a1b9c1b9ea47f9331db754fcfb6fb8f9def275
kangjoohyeong <bles@kakao.com>
2023-07-13 13:37:01 +0900

    rename: password

--- workspace/myapi/domain/chatroom/chatroom_schema.py
+++ workspace/myapi/domain/chatroom/chatroom_schema.py
@@ -5,8 +5,7 @@ from pydantic import BaseModel
 # Chatroom 스키마
 class Chatroom(BaseModel):
     id: int
-    question: str
-    answer: str
+    user_id: int
     create_date: datetime.datetime
     
     # CharRoom 모델의 항목들이 자동으로 Chatroom 스키마로 매핑 됨
--- workspace/myapi/domain/user/user_schema.py
+++ workspace/myapi/domain/user/user_schema.py
@@ -3,19 +3,19 @@ from pydantic import BaseModel, validator, EmailStr
 
 class UserCreate(BaseModel):
     username: str
-    password1: str
-    password2: str
+    password: str
+    confirm_password: str
     email: EmailStr
 
-    @validator('username', 'password1', 'password2', 'email')
+    @validator('username', 'password', 'confirm_password', 'email')
     def not_empty(cls, v):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
         return v
 
-    @validator('password2')
+    @validator('confirm_password')
     def passwords_match(cls, v, values):
-        if 'password1' in values and v != values['password1']:
+        if 'password' in values and v != values['password']:
             raise ValueError('비밀번호가 일치하지 않습니다')
         return v
 
